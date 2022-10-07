18519376bffc49355a9d1fec2e90a4647df11b9f
perumalComorins <perumal@mail.comorins.com>
2022-10-07 10:59:42 +0530

    Issue fixed by Perumal
    Issue fixed by Perumal

--- website_catalogue/model.php
+++ website_catalogue/model.php
@@ -1,12 +1,14 @@
 <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
         
     <div class="modal-dialog container-80 requestform-popup-page">
-        <h2 class="form-heading">Request for a new sector</h2>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+        <div class="modal-content requestmodal_content">
+            <div class="modal-header border-0">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
-        <div class="modal-content requestmodal_content">
+            </div>
             <form>
+                <h2 class="form-heading">Request for a new sector</h2>
                 <div class="form-group">
                     <label for="exampleFormControlInput1 formleft-sub-title">Give a name to the sector that you wish.</label>
                     <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="">
@@ -23,10 +25,36 @@
                     <label for="exampleFormControlInput1 formleft-sub-title">Can we contact you for further details ?</label>
                     <input type="email" class="form-control emailsec" id="exampleFormControlInput1" placeholder="Your e-mail id here.">
                 </div>
-            </form>
-            <h3 class="request-condent ">This site is protected by reCAPTCHA and the Google<span style="color:white;"> Privacy Policy </span>and <span style="color:white;">Terms of Conditions</span> apply.
-            <div class="row reset-margin pt-5">
-                <div class="col-6 robotimage reset-padding g-recaptcha"data-sitekey="6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI"></div>
-               
 
-                <div class="col-6 submit reset-padding"><button type="button" class="click-mail">submit</button></div>
+                <h3 class="request-condent ">This site is protected by reCAPTCHA and the Google<span style="color:white;"> Privacy Policy </span>and <span style="color:white;">Terms of Conditions</span> apply.</h3>
+                <div class="row reset-margin pt-5">
+                    <div class="col-6 robotimage reset-padding g-recaptcha"data-sitekey="6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI"></div>
+                    <div class="col-6 submit reset-padding"><button type="button" class="click-mail">submit</button></div>
+                </div>
+            </form>
+            
+        </div>
+    </div>
+</div>
+
+
+    <!-- <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+        <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+        <div class="modal-body">
+            ...
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Save changes</button>
+        </div>
+        </div>
+    </div>
+    </div> -->
+
