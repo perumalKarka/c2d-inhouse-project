d339f0ec263a9af5df2e486b49e567bdc68e6bff
indhucomorins <indhu@mail.comorins.com>
2022-10-11 12:40:30 +0530

    branch CIP-9
    buy-copyright-model

--- website_catalogue/model-v2.php
+++ website_catalogue/model-v2.php
@@ -138,3 +138,67 @@
         </div>
     </div>
 </div>
+<!-- fill the customer details -->
+<div class="modal fade" id="exampleModal-3" tabindex="-1" aria-labelledby="exampleModalLabel" data-backdrop="static" aria-hidden="true">
+    <div class="modal-dialog container-80 requestform-popup-page">
+        <div class="modal-content requestmodal_content logo-name-overlay">
+            <div class="requestmodal_content">
+                <div class="modal-header border-0">
+                    <button type="button" class="close" data-dismiss="modal"  aria-label="Close">
+                        <img src="images/closeicon.png">
+                    </button>
+                </div> 
+                <form class="request-popup-form">
+                    <h2 class="form-heading pb-2">fill in the customer details</h2>
+                        <div class="form-row reset-margin submission">
+                            <div class="form-group col-md-6 pt-5">
+                            <label for="inputEmail4">First Name <span class="required">*</span></label>
+                            <input type="text" class="form-control" id="inputEmail4">
+                            </div>
+                            <div class="form-group col-md-6 pt-5">
+                            <label for="inputPassword4">Last Name <span class="required">*</span></label>
+                            <input type="text" class="form-control" id="inputPassword4">
+                            </div>
+                            <div class="form-group col-md-6 pt-5">
+                            <label for="inputPassword4">Company Name <span class="required">*</span></label>
+                            <input type="text" class="form-control" id="inputPassword4">
+                            </div>
+                            <div class="form-group col-md-6 pt-5">
+                            <label for="inputPassword4">VAT Number <span class="required">*</span></label>
+                            <input type="number" class="form-control" id="inputPassword4">
+                            </div>
+                            <div class="form-group col-md-6 pt-5">
+                            <label for="inputPassword4">Position in the company <span class="required">*</span></label>
+                            <input type="text" class="form-control" id="inputPassword4">
+                            </div>
+                            <div class="form-group col-md-6 pt-5">
+                            <label for="inputPassword4">Size of the company<span class="required">*</span></label>
+                            <input type="text" class="form-control" id="inputPassword4">
+                            </div>
+                            <div class="form-group col-md-6 pt-5">
+                            <label for="inputPassword4">Phone Number<span class="required">*</span></label>
+                            <input type="number" class="form-control" id="inputPassword4">
+                            </div>
+                            <div class="form-group col-md-6 pt-5">
+                            <label for="inputPassword4">Email Address <span class="required">*</span></label>
+                            <input type="text" class="form-control" id="inputPassword4">
+                            </div>
+                            <div class="form-group email-message col-lg-12 pt-5">
+                            <label for="inputAddress">Message</label>
+                            <textarea class="form-control message" id="exampleFormControlTextarea1" rows="3"></textarea>
+                            </div>
+                        </div>
+                        <div class="row reset-margin message-subcondent">
+                            <h3 class="message-condent">This site is protected by reCAPTCHA and the Google<span style="color:white;"> Privacy Policy </span>and <span style="color:white;">Terms of Conditions</span> apply.</h3>
+                            <div class="col-6 robotimage reset-padding g-recaptcha"data-sitekey="6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI"></div>
+                            <div class="col-6 submit reset-padding align-self-center">
+                                <label for="inputPassword4" class="field"> <span class="required">*</span> Fields required</label>
+                                <button type="button" class="click-mail">Contact us now!</button>
+                            </div>
+                        </div>
+                </form>
+            </div>
+            
+        </div>
+    </div>
+</div>
