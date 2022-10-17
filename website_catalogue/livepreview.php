6dfec9e96f2b326f98f0179a12dd9ac28b16ec18
perumalComorins <perumal@mail.comorins.com>
2022-10-17 16:08:30 +0530

    CIP-14 Left side fixed tab Integration
    First level

--- website_catalogue/livepreview.php
+++ website_catalogue/livepreview.php
@@ -10,6 +10,7 @@
     <link rel="stylesheet" href="css/owl.carousel.min.css">
     <link rel="stylesheet" href="css/owl.theme.default.min.css">
     <link rel="stylesheet" href="fonts/icomoon/style.css">
+    <link rel="stylesheet" type="text/css" href="css/style4.css" />
     <link rel="stylesheet" href="css/theme.css" />
     <link rel="stylesheet" href="css/mobile_style.css" />
     <link rel="icon" href="images/favicon-ico.png" sizes="32x32" />
@@ -60,7 +61,69 @@
             <img src="images/image.jpg" class="body-condent-image">
         </section>
         
-        
+        <!-- Tab component -->
+        <section class="tabs">
+            <div class="tab-group">
+                <input id="tab-1" type="radio" name="radio-set" class="tab-selector-1"/>
+                <div for="tab-1" class="label tab-label-1">
+                    <i class="fa fa-file d-block"></i>
+                    Pages
+                </div>
+                <input id="tab-2" type="radio" name="radio-set" class="tab-selector-2" />
+                <div for="tab-2" class="label tab-label-2">
+                    <i class="fa fa-image d-block"></i>
+                    Logo
+                </div>
+                <input id="tab-3" type="radio" name="radio-set" class="tab-selector-3" />
+                <div for="tab-3" class="label tab-label-3">
+                    <img class="d-block i-icon" src="images/color-pic.png" width="25" height="25" />
+                    Custom
+                </div>
+                <input id="tab-4" type="radio" name="radio-set" class="tab-selector-4" />
+                <div for="tab-4" class="label tab-label-4">
+                    <img class="d-block i-icon" src="images/footer-icon.png" width="29" height="33" />
+                    Footer
+                </div>
+                <input id="tab-5" type="radio" name="radio-set" class="tab-selector-5" />
+                <div for="tab-5" class="label tab-label-5">
+                    <i class="fa fa-pencil d-block"></i>
+                    Business name
+                </div>
+            </div>
+            
+            <div class="content">
+                <div class="content-1">
+                    <h2>About us</h2>
+                    <p>You think water moves fast? You should see ice. It moves like it has a mind. Like it knows it killed the world once and got a taste for murder. After the avalanche, it took us a week to climb out. Now, I don't know exactly when we turned on each other, but I know that seven of us survived the slide... and only five made it out. Now we took an oath, that I'm breaking now. We said we'd say it was the snow that killed the other two, but it wasn't. Nature is lethal but it doesn't hold a candle to man.</p>
+                    <h3>How we work</h3>
+                    <p>Like you, I used to think the world was this great place where everybody lived by the same standards I did, then some kid with a nail showed me I was living in his world, a world where chaos rules not order, a world where righteousness is not rewarded. That's Cesar's world, and if you're not willing to play by his rules, then you're gonna have to pay the price. </p>
+                </div>
+                <div class="content-2">
+                    <h2>Services</h2>
+                    <p>Do you see any Teletubbies in here? Do you see a slender plastic tag clipped to my shirt with my name printed on it? Do you see a little Asian child with a blank expression on his face sitting outside on a mechanical helicopter that shakes when you put quarters in it? No? Well, that's what you see at a toy store. And you must think you're in a toy store, because you're here shopping for an infant named Jeb.</p>
+                    <h3>Excellence</h3>
+                    <p>Like you, I used to think the world was this great place where everybody lived by the same standards I did, then some kid with a nail showed me I was living in his world, a world where chaos rules not order, a world where righteousness is not rewarded. That's Cesar's world, and if you're not willing to play by his rules, then you're gonna have to pay the price. </p>
+                </div>
+                <div class="content-3">
+                    <h2>Portfolio</h2>
+                    <p>The path of the righteous man is beset on all sides by the iniquities of the selfish and the tyranny of evil men. Blessed is he who, in the name of charity and good will, shepherds the weak through the valley of darkness, for he is truly his brother's keeper and the finder of lost children. And I will strike down upon thee with great vengeance and furious anger those who would attempt to poison and destroy My brothers. And you will know My name is the Lord when I lay My vengeance upon thee.</p>
+                    <h3>Examples</h3>
+                    <p>Now that we know who you are, I know who I am. I'm not a mistake! It all makes sense! In a comic, you know how you can tell who the arch-villain's going to be? He's the exact opposite of the hero. And most times they're friends, like you and me! I should've known way back when... You know why, David? Because of the kids. They called me Mr Glass. </p>
+                </div>
+                <div class="content-4">
+                    <h2>Contact</h2>
+                    <p>You see? It's curious. Ted did figure it out - time travel. And when we get back, we gonna tell everyone. How it's possible, how it's done, what the dangers are. But then why fifty years in the future when the spacecraft encounters a black hole does the computer call it an 'unknown entry event'? Why don't they know? If they don't know, that means we never told anyone. And if we never told anyone it means we never made it back. Hence we die down here. Just as a matter of deductive logic.</p>
+                    <h3>Get in touch</h3>
+                    <p>Well, the way they make shows is, they make one show. That show's called a pilot. Then they show that show to the people who make shows, and on the strength of that one show they decide if they're going to make more shows. Some pilots get picked and become television programs. Some don't, become nothing. She starred in one of the ones that became nothing. </p>
+                </div>
+                <div class="content-5">
+                    <h2>Contact 5</h2>
+                    <p>You see? It's curious. Ted did figure it out - time travel. And when we get back, we gonna tell everyone. How it's possible, how it's done, what the dangers are. But then why fifty years in the future when the spacecraft encounters a black hole does the computer call it an 'unknown entry event'? Why don't they know? If they don't know, that means we never told anyone. And if we never told anyone it means we never made it back. Hence we die down here. Just as a matter of deductive logic.</p>
+                    <h3>Get in touch</h3>
+                    <p>Well, the way they make shows is, they make one show. That show's called a pilot. Then they show that show to the people who make shows, and on the strength of that one show they decide if they're going to make more shows. Some pilots get picked and become television programs. Some don't, become nothing. She starred in one of the ones that became nothing. </p>
+                </div>
+            </div>
+        </section>    
 
 
         <!-- footer -->
@@ -78,7 +141,10 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <script src="js/owl.carousel.js"></script>
     <script src="js/jquery.sticky.js"></script>
+    <script type="text/javascript" src="js/modernizr.custom.04022.js"></script>
     <script src="js/mobile_menu.js"></script>
     <script src="js/main.js"></script>
+
+    
 </body>
 </html>
