a6ee5dd171b68d89fafe007472f79ef7b1d32932
indhucomorins <indhu@mail.comorins.com>
2022-11-11 12:56:55 +0530

    branch CIP-21
    send-message-popup

--- website_catalogue/model-v2.php
+++ website_catalogue/model-v2.php
@@ -7,52 +7,55 @@
                         <img src="images/closeicon.png">
                     </button>
                 </div> 
-                <form class="request-popup-form">
+                <form class="request-popup-form send-us-popup-message-form">
                     <h2 class="form-heading pb-2">Send us a message!</h2>
                         <div class="form-row reset-margin submission">
-                            <div class="form-group col-md-6 pt-5">
+                            <div class="form-group col-md-6 pt-2">
                             <label for="inputEmail4">First Name <span class="required">*</span></label>
                             <input type="text" class="form-control" id="inputEmail4">
                             </div>
-                            <div class="form-group col-md-6 pt-5">
+                            <div class="form-group col-md-6 pt-2">
                             <label for="inputPassword4">Last Name <span class="required">*</span></label>
                             <input type="text" class="form-control" id="inputPassword4">
                             </div>
-                            <div class="form-group col-md-6 pt-5">
+                            <div class="form-group col-md-6 pt-2">
                             <label for="inputPassword4">Company Name <span class="required">*</span></label>
                             <input type="text" class="form-control" id="inputPassword4">
                             </div>
-                            <div class="form-group col-md-6 pt-5">
+                            <div class="form-group col-md-6 pt-2">
                             <label for="inputPassword4">VAT Number <span class="required">*</span></label>
                             <input type="number" class="form-control" id="inputPassword4">
                             </div>
-                            <div class="form-group col-md-6 pt-5">
+                            <div class="form-group col-md-6 pt-2">
                             <label for="inputPassword4">Position in the company <span class="required">*</span></label>
                             <input type="text" class="form-control" id="inputPassword4">
                             </div>
-                            <div class="form-group col-md-6 pt-5">
+                            <div class="form-group col-md-6 pt-2">
                             <label for="inputPassword4">Size of the company<span class="required">*</span></label>
                             <input type="text" class="form-control" id="inputPassword4">
                             </div>
-                            <div class="form-group col-md-6 pt-5">
+                            <div class="form-group col-md-6 pt-2">
                             <label for="inputPassword4">Phone Number<span class="required">*</span></label>
                             <input type="number" class="form-control" id="inputPassword4">
                             </div>
-                            <div class="form-group col-md-6 pt-5">
+                            <div class="form-group col-md-6 pt-2">
                             <label for="inputPassword4">Email Address <span class="required">*</span></label>
                             <input type="text" class="form-control" id="inputPassword4">
                             </div>
-                            <div class="form-group email-message col-lg-12 pt-5">
+                            <div class="form-group email-message col-lg-12 pt-2">
                             <label for="inputAddress">Message</label>
                             <textarea class="form-control message" id="exampleFormControlTextarea1" rows="3"></textarea>
                             </div>
                         </div>
-                        <div class="row reset-margin message-subcondent">
-                            <h3 class="message-condent">This site is protected by reCAPTCHA and the Google<span style="color:white;"> Privacy Policy </span>and <span style="color:white;">Terms of Conditions</span> apply.</h3>
-                            <div class="col-6 robotimage reset-padding g-recaptcha"data-sitekey="6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI"></div>
-                            <div class="col-6 submit reset-padding align-self-center">
-                                <label for="inputPassword4" class="field"> <span class="required">*</span> Fields required</label>
-                                <button type="button" class="click-mail">Contact us now!</button>
+                        <div class="row reset-margin message-subcondent sendmesage-subcondent">
+                        <label for="inputPassword4" class="field d-none d-md-block d-lg-none "> <span class="required">*</span> Fields required</label>
+                            <h3 class="message-condent col-xl-12 col-lg-8 col-md-12 ">This site is protected by reCAPTCHA and the Google<span style="color:white;"> Privacy Policy </span>and <span style="color:white;">Terms of Conditions</span> apply.</h3>
+                            <label for="inputPassword4" class="field d-none d-lg-block  d-xl-none"> <span class="required">*</span> Fields required</label>
+                            
+                            <div class="col-xl-6 col-lg-6 col-12 robotimage reset-padding g-recaptcha send-recaptche"data-sitekey="6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI"></div>
+                            <div class="col-xl-6 col-lg-6 col-12 submit send-button reset-padding align-self-center">
+                                <label for="inputPassword4" class="field d-none d-xl-block"> <span class="required">*</span> Fields required</label>
+                                <button type="button" class="click-mail ">Contact us now!</button>
                             </div>
                         </div>
                 </form>
